15832f5c83319798545d1d63b21d32bd2aa4201a
phucabc83 <tailam164@gmail.com>
2025-12-09 20:07:06 +0700

    update structure project

---
+++ Trello_Automation/Test Suites/Module7_WorkSpaceManagement_CardFilter/TS_WORKSPACEMANAGEMENT.ts
@@ -0,0 +1,189 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_WORKSPACEMANAGEMENT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5980e8e6-d549-4997-8fdb-027ff83bf998</testSuiteGuid>
+   <testCaseLink>
+      <guid>7c037c83-c9ac-4fb0-b1a2-9f71512e435d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ded0a596-f972-4771-890d-4f0d682ceea1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_02</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>de150650-dd82-46a9-831e-24605384671c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_03</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6b150ffa-96fc-4e56-b457-fec7c4c4e51d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_04</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f477e3ab-aab3-4bec-a585-666d6fb77ce3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_05</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>36cf1335-3483-4731-a9cb-d98df0a4261b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_06</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>35013866-728a-4824-a7a1-ee2c4ff8f02f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_07</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7f886997-5d68-4d38-b48d-ef723901f8e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_08</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ad840c61-3e53-4c19-b3ef-e634af2787fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_09</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>80260ffb-9d1c-42b9-95d0-e60a73ad181b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_10</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>73d83438-5403-4d43-b5d5-890746391e2b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_11</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>19eb4ee3-cc9b-495c-9454-a42eb70dc420</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_12</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1d7a67fb-4cbb-4c76-9ebe-a8c81bb212cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_ADD_13</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aa5d7a2f-800d-4e60-ae2b-4a878fe75828</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ad294c30-00b6-4d8b-a891-6de13e8c25e4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_02</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>45fa440a-cdee-4818-a247-e1663d13c4e5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_03</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c5564100-e32c-46c1-afb6-9ff5ce6b8008</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_04</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c8c2b18f-253b-4076-8f45-a442dc2b9ee1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_05</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6239ec2-3c5d-4399-a72a-4b5ae58cf305</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_06</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ab91b345-43b8-4d07-b210-c667b2781c5c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_07</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f18fea79-16f0-4ab3-99de-14f26e3abcb7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_08</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a2b34a94-007c-4007-9a48-5f1da1369e08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_EDIT_09</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38f8e9a3-26ba-48b8-ae85-53303481e707</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_DELETE_01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cbe1f559-b132-4845-b041-c05d2dd6bc15</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_DELETE_02</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>edf9c71e-e1c0-4bf4-b011-1d49f40df42a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Module7_WorkSpaceManagement_CardFilter/TC_WORKSPACE_DELETE_03</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
